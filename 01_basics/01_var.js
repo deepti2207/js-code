7fd0b9c5984f67a87daf8cead5c95478d5b964d8
Deepti rai <144608021+deepti2207@users.noreply.github.com>
2024-05-31 08:21:03 +0000

    container config done

---
+++ 01_basics/01_var.js
@@ -0,0 +1,16 @@
+const accountId = 142247; // in const not changed the value after assigning
+let accountEmail = "deeptirai0712@gmail.com" // can changed
+var accountPassword = "1524" // let used more than var , ignore used var
+accountCity = "lucknow" // not prefer in js
+let accountState
+
+accountEmail = "deeps07@gmail.com"
+accountPassword = "5436"
+accountCity = "noida"
+
+console.log(accountId);
+//console.log(accountEmail);
+//console.log(accountPassword);
+// how many time used console.log so that's why 
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
