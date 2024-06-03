be6af5fad73854bf71c5ed57b6eaecf3b5dc10a3
Deepti rai <144608021+deepti2207@users.noreply.github.com>
2024-06-03 05:00:30 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,9 @@
+let score = "32"
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+//"32" = output will be 32
+// "32ang" = o/p will be not a number
+// "deepti" = o/p will be not a number
+// true = o/p will be 1
